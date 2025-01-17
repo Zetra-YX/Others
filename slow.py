9afef7c7ed82543dd83bc77b0afca602caa19bff
Zetra <94852849+Zetra-YX@users.noreply.github.com>
2025-01-16 23:09:36 -0300

    Update slow.py

--- slow.py
+++ slow.py
@@ -1,4 +1,6 @@
 games = {
-    "1": ("Valorant - Aim ( Notepad )", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&authuser=0&confirm=t&uuid=e3a99e11-73ec-4334-b7c1-26b88a82e676&at=AO7h07f7bKnqeNndMqpvyCBIRSYH%3A1727139311235", "online", "6.3.5"),
-    "2": ("Valorant - Aim ( Menu )", "https://drive.usercontent.google.com/download?id=1gxGSHa6YibG4EzfSaWLohRxKKiyPVAPW&export=download&authuser=0&confirm=t&uuid=f12314d7-9c9c-4385-a53c-fdd433cfd10e&at=APvzH3qzniW5txug0mpyRnWn7bYk%3A1734114373553", "online", "6.3.5")
+    "1": ("Valorant - Aim", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&authuser=0&confirm=t&uuid=e3a99e11-73ec-4334-b7c1-26b88a82e676&at=AO7h07f7bKnqeNndMqpvyCBIRSYH%3A1727139311235", "online", "6.3.5"),
+    "2": ("DayZ", "https://drive.usercontent.google.com/download?id=14WNWjEmu8R3SvI8TB4tG1VVAC_8KDnQ2&export=download&confirm=t&uuid=25b8a41d-4c3c-4d4f-9e10-4f84a7149755", "online", "6.4.4"),
+    "3": ("Apex", "https://drive.usercontent.google.com/download?id=16vD7pI1eVGR2FiaBPxaHCbpn_PIGW9qy&export=download&confirm=t&uuid=e060383b-9b8a-47d3-b649-f7430bcee3bf", "online", "6.5.6"),
+    "4": ("Squad", "https://drive.usercontent.google.com/download?id=1Supe4ntHMvXLpYL_m2VvAIfUWbzPU4Tf&export=download&confirm=t&uuid=436a8a99-4945-4984-8f78-fe8dcb8d36d2", "online", "6.1.8")
 }
