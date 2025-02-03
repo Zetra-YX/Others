6353de71bb4521c271db40bcac1dccb991abf46f
Zetra <94852849+Zetra-YX@users.noreply.github.com>
2025-02-03 12:54:44 -0300

    Update Reseller.py

--- Reseller.py
+++ Reseller.py
@@ -1,5 +1,6 @@
 games = {
     "1": ("Valorant - Aim", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&confirm=t&uuid=e0753063-345d-4b5b-8c79-5bbbdd303fa4", "online", "VIP"),
     "2": ("Valorant - Memory", "https://drive.usercontent.google.com/download?id=1gxGSHa6YibG4EzfSaWLohRxKKiyPVAPW&export=download&authuser=0&confirm=t&uuid=f12314d7-9c9c-4385-a53c-fdd433cfd10e&at=APvzH3qzniW5txug0mpyRnWn7bYk%3A1734114373553", "online", "VIP"),
-    "3": ("Valorant - Full", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&authuser=0&confirm=t&uuid=e3a99e11-73ec-4334-b7c1-26b88a82e676&at=AO7h07f7bKnqeNndMqpvyCBIRSYH%3A1727139311235", "online", "VIP")
+    "3": ("Valorant - Full", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&authuser=0&confirm=t&uuid=e3a99e11-73ec-4334-b7c1-26b88a82e676&at=AO7h07f7bKnqeNndMqpvyCBIRSYH%3A1727139311235", "online", "VIP"),
+    "4": ("Delta Force", "https://drive.usercontent.google.com/download?id=1JpAS3whb8_e7FcPFjc5uiEeUSuCczF3C&export=download&confirm=t&uuid=231bfd74-e820-4d21-bfd1-dcf58649697a", "online", "VIP")
 }
