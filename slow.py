8dc64a5b4971e9dc109fde580e1ee56a92447b0d
Zetra <94852849+Zetra-YX@users.noreply.github.com>
2025-02-04 01:31:15 -0300

    Update slow.py

--- slow.py
+++ slow.py
@@ -1,6 +1,8 @@
 games = {
-    "1": ("Valorant - Aim", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&confirm=t&uuid=e0753063-345d-4b5b-8c79-5bbbdd303fa4", "online", "6.3.5"),
-    "2": ("DayZ", "https://drive.usercontent.google.com/download?id=14WNWjEmu8R3SvI8TB4tG1VVAC_8KDnQ2&export=download&confirm=t&uuid=25b8a41d-4c3c-4d4f-9e10-4f84a7149755", "online", "6.4.4"),
-    "3": ("Apex", "https://drive.usercontent.google.com/download?id=16vD7pI1eVGR2FiaBPxaHCbpn_PIGW9qy&export=download&confirm=t&uuid=e060383b-9b8a-47d3-b649-f7430bcee3bf", "online", "6.5.6"),
-    "4": ("Squad", "https://drive.usercontent.google.com/download?id=1Supe4ntHMvXLpYL_m2VvAIfUWbzPU4Tf&export=download&confirm=t&uuid=436a8a99-4945-4984-8f78-fe8dcb8d36d2", "online", "6.1.8")
+    "1": ("Valorant - Aim", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&confirm=t&uuid=e0753063-345d-4b5b-8c79-5bbbdd303fa4", "online", "VIP"),
+    "2": ("Valorant - Memory", "https://drive.usercontent.google.com/download?id=1gxGSHa6YibG4EzfSaWLohRxKKiyPVAPW&export=download&authuser=0&confirm=t&uuid=f12314d7-9c9c-4385-a53c-fdd433cfd10e&at=APvzH3qzniW5txug0mpyRnWn7bYk%3A1734114373553", "online", "VIP"),
+    "3": ("Valorant - Full", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&authuser=0&confirm=t&uuid=e3a99e11-73ec-4334-b7c1-26b88a82e676&at=AO7h07f7bKnqeNndMqpvyCBIRSYH%3A1727139311235", "online", "VIP"),
+    "4": ("Delta Force", "https://drive.usercontent.google.com/download?id=1JpAS3whb8_e7FcPFjc5uiEeUSuCczF3C&export=download&confirm=t&uuid=231bfd74-e820-4d21-bfd1-dcf58649697a", "online", "VIP"),
+    "5": ("Marvel Rivals", "https://drive.usercontent.google.com/download?id=11ckuxgoMGudG_kxDXQtl0e_32Jubqn1Y&export=download&confirm=t&uuid=ea36e10c-2fa4-4737-a16c-2764cf0d2838", "online", "VIP"),
+    "6": ("Pubg", "https://drive.usercontent.google.com/download?id=1-x41pMplW0p8YkcA4biEZ77Vh9n66Ur8&export=download&confirm=t&uuid=23967058-6785-4f39-a1f8-cd1d07806988", "online", "VIP")
 }
