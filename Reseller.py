2d4227c57b7602d67cd24f9a40c8a194dcb87c2e
Zetra <94852849+Zetra-YX@users.noreply.github.com>
2025-02-09 23:52:12 -0300

    Update Reseller.py

--- Reseller.py
+++ Reseller.py
@@ -4,5 +4,5 @@ games = {
     "3": ("Valorant - Full", "https://drive.usercontent.google.com/download?id=1AiE4eCGejuuZqzeCpmEagDC_N-4nhlhG&export=download&confirm=t&uuid=e65c23c7-8b36-42cb-a36d-217eefba2ae8&at=AIrpjvMruZ08afeYdLWKqeaMYhRu%3A1738809695071", "online", "VIP"),
     "4": ("Delta Force", "https://drive.usercontent.google.com/download?id=1JpAS3whb8_e7FcPFjc5uiEeUSuCczF3C&export=download&confirm=t&uuid=231bfd74-e820-4d21-bfd1-dcf58649697a", "online", "VIP"),
     "5": ("Marvel Rivals", "https://drive.usercontent.google.com/download?id=11ckuxgoMGudG_kxDXQtl0e_32Jubqn1Y&export=download&confirm=t&uuid=ea36e10c-2fa4-4737-a16c-2764cf0d2838", "online", "VIP"),
-    "6": ("Pubg", "https://drive.usercontent.google.com/download?id=1-x41pMplW0p8YkcA4biEZ77Vh9n66Ur8&export=download&confirm=t&uuid=e995ea70-07ad-4b89-8d10-b22c2fe43c4b", "online", "VIP")
+    "6": ("Pubg", "https://drive.usercontent.google.com/download?id=1-x41pMplW0p8YkcA4biEZ77Vh9n66Ur8&export=download&confirm=t&uuid=e995ea70-07ad-4b89-8d10-b22c2fe43c4b", "offline", "VIP")
 }
